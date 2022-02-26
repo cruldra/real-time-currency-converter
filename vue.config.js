12b2e83ddd2778eeece3501dff6c0768f673ab79
dongjak <cruldra@gmail.com>
2022-02-26 17:06:28 +0800

    11

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require("@vue/cli-service");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require("path");
+const pages = require("./pages");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
+
+module.exports = {
+  pages,
+  filenameHashing: false,
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve("manifest.json"),
+            to: `${path.resolve("dist")}/manifest.json`,
+          },
+        ],
+      }),
+    ],
+  },
+};
