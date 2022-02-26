a2ea70987d7c7b921119934b0682491a6a8409b4
dongjak <cruldra@gmail.com>
2022-02-26 18:14:00 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,9 @@ const { defineConfig } = require("@vue/cli-service");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 const path = require("path");
 const pages = require("./pages");
+const AutoImport = require("unplugin-auto-import/webpack");
+const Components = require("unplugin-vue-components/webpack");
+const { ElementPlusResolver } = require("unplugin-vue-components/resolvers");
 module.exports = defineConfig({
   transpileDependencies: true,
 });
@@ -18,6 +21,12 @@ module.exports = {
             to: `${path.resolve("dist")}/manifest.json`,
           },
         ],
+      }),
+      AutoImport({
+        resolvers: [ElementPlusResolver()],
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()],
       }),
     ],
   },
